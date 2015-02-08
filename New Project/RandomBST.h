9ebb1188533fe8b638d990068581447a1f1bba76
Shashank Devan <shashankdevan@gmail.com>
2015-02-08 11:01:17 -0500

    Update RandomBST.h
    added nextInt()

--- New Project/RandomBST.h
+++ New Project/RandomBST.h
@@ -1,36 +1,33 @@
-#include <iostream>
-#include <stdlib.h>
-#include <queue>
-
 using namespace std; 
 
-  class node
-   {
-       public:
-       int key;  
-       int size; 
-       node* left; 
-       node* right; 
-       node(int k);
-   };
-   
-   class RandomisedBST {
-       node* root;
-       node* r;
-       int height;
-       int randomisedHeight;
-       public:
-       RandomisedBST();
-       int getSize(node* p);
-       void adjustSize(node* p);
-       node* rotateRight(node* p);
-       node* rotateLeft(node* p);
-       node* insertAtRoot(node* p, int k);
-       void insert(int k);
-       void BSTInsert(int k);
-       node * BSTInsert(node*p, int k);
-       void randomisedInsert(int k);
-       node* randomisedInsert(node* p, int k);
-       void levelOrderPrint();
-       int levelOrderPrint(node* p);
-   };
+class node
+{
+   public:
+   int key;  
+   int size; 
+   node* left; 
+   node* right; 
+   node(int k);
+};
+
+class RandomisedBST {
+   node* root;
+   node* r;
+   int height;
+   int randomisedHeight;
+   public:
+   RandomisedBST();
+   int getSize(node* p);
+   void adjustSize(node* p);
+   node* rotateRight(node* p);
+   node* rotateLeft(node* p);
+   int nextInt();
+   node* insertAtRoot(node* p, int k);
+   void insert(int k);
+   void BSTInsert(int k);
+   node * BSTInsert(node*p, int k);
+   void randomisedInsert(int k);
+   node* randomisedInsert(node* p, int k);
+   void levelOrderPrint();
+   int levelOrderPrint(node* p);
+};
